cbaa83e317127df32a4b2371cbe4b30629782f07
Brian Mwenda <brianmwenda255@gmail.com>
2022-10-18 16:50:22 +0300

    link components

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import  About  from './components/about/About'
+import Contacts from './components/contacts/Contacts'
+import Experience from './components/experience/Experience'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import Portfolio from './components/portfolio/Portfolio'
+import Services from './components/services/Services'
+import Testimonials from './components/testimonials/Testimonials'
+
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Nav/>
+    <About/>
+    <Experience/>
+    <Services/>
+    <Portfolio/>
+    <Testimonials/>
+    <Testimonials/>
+    <Contacts/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
