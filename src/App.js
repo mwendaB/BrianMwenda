76526172cd00156f81c3c13d8a7d29ad26674f7e
Brian Mwenda <brianmwenda255@gmail.com>
2022-11-05 12:28:54 +0300

    create the project section

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ import Portfolio from "./components/Portfolio/Portfolio";
 import Testimonial from "./components/Testimonials/Testimonial";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import Projects from "./components/project/Projects";
 import { useContext } from "react";
 import { themeContext } from "./Context";
+
 function App() {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
@@ -27,6 +29,7 @@ function App() {
       <Experience />
       <Works />
       <Portfolio />
+      <Projects/>
       <Testimonial />
       <Contact />
       <Footer />
