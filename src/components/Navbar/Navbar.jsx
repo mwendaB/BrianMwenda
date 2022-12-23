8682b566719f5e3930d4216c799f750389f8a9d1
Brian Mwenda <brianmwenda255@gmail.com>
2022-12-23 13:59:03 +0300

    finish working the project

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,52 +1,95 @@
-import React from "react";
-import Toggle from "../Toggle/Toggle";
-import "./Navbar.css";
-import { Link } from "react-scroll";
-const navbar = () => {
+import React, { useState, useEffect } from 'react';
+import "./Navbar.scss";
+// eslint-disable-next-line no-unused-vars
+import { animate, motion } from 'framer-motion';
+import {BsLinkedin} from 'react-icons/bs';
+import {FaGithub} from 'react-icons/fa';
+import {BsInstagram} from 'react-icons/bs';
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { navLinks } from '../../Data';
+// import { socialIcons } from '../../Data';
+const Navbar = () => {
+    
+    const [scroll, setScroll] = useState(false);
+    const [toggle, setToggle] = useState(false)
+    
+    const menuVariants = {
+        hidden: {
+            scale: 0
+        },
+        visible: {
+            scale: 50,
+            transition: {
+                type: "tween",
+                duration: 0.5,
+            }
+        }
+    }
+    const navLinkVariants = {
+        hidden: {
+            display: "none",
+            opacity: 0
+        },
+        visible: {
+            opacity: 1,
+            y: -30,
+            transition: {
+                delay: 0.7
+            }
+        }
+    }
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            setScroll(window.scrollY > 20)
+        })
+    },[])
   return (
-    <div className="n-wrapper" id="Navbar">
-      {/* left */}
-      <div className="n-left">
-        <div className="n-name">Mwenda</div>
-        <Toggle />
-      </div>
-      {/* right */}
-      <div className="n-right">
-        <div className="n-list">
-          <ul style={{ listStyleType: "none" }}>
-            <li>
-              <Link activeClass="active" to="Navbar" spy={true} smooth={true}>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="services" spy={true} smooth={true}>
-                Serivces
-              </Link>
-            </li>
-            <li>
-              <Link to="works" spy={true} smooth={true}>
-                Experience
-              </Link>
-            </li>
-            <li>
-              <Link to="portfolio" spy={true} smooth={true}>
-                Protfolio
-              </Link>
-            </li>
-            <li>
-              <Link to="testimonial" spy={true} smooth={true}>
-                Testimonial
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <Link to="contact" spy={true} smooth={true}>
-        <button className="button n-button">Contact</button>
-        </Link>
-      </div>
-    </div>
-  );
-};
+      <motion.div
+          initial={{ y: -25 }}
+          animate={{ y: -5 }}
+          transition={{duration: 0.5}}
+          className={scroll ? "header active" : "header"}>
+          <div className="Nav_container">
+              <div className="logo">
+                    <h3>M</h3>
+              </div>
+              <ul
+                      className="nav_links"
+                   >
+                  {navLinks.map((navlink,index) => {
+                      return <li key={index}><a href={`#${navlink}`}>{navlink}</a></li>
+                  })}
+              </ul>
+              <div className="social_icons" >
+              
+              <a href="https://www.linkedin.com/in/brian-mwenda-73a926228/"><BsLinkedin/></a>
+               <a href="https://github.com/mwendaB" size={"3rem"}> <FaGithub/></a>
+               <a href="https://www.instagram.com/its_mwend_a/"> <BsInstagram/></a>
+              </div>
+              <div className="menu" >
+                  <HiMenuAlt4 onClick={() => {setToggle(true)}}/>
+              </div>
+              <motion.div className="closeMenu"
+                          variants={menuVariants}
+                          initial="hidden" animate= {toggle ?"visible": "hidden"}
+                      >
+              </motion.div>
+              
+          <motion.div
+                  variants={navLinkVariants}
+                  animate={toggle ? "visible" : "hidden"}
+                  className="menuX"
+              >
+                  <HiX onClick={() => setToggle(false)}/>
+                  {navLinks.map((navlink,index) => {
+                      return <li
+                          key={index}><a
+                          href={`${navlink}`} onClick={() => setToggle(false)}>{navlink}</a></li>
+                  })}
+              </motion.div>
+          </div>
+    </motion.div>
+  )
+}
 
-export default navbar;
+export default Navbar
