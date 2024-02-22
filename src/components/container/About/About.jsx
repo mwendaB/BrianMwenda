bc3ba5719e4b14ee32f8af8adb0e03a30b42e657
Brian Mwenda <brianmwenda255@gmail.com>
2024-02-22 12:40:38 +0000

    create new files

--- src/components/container/About/About.jsx
+++ src/components/container/About/About.jsx
@@ -1,19 +1,54 @@
-import React from 'react'
 import "./About.scss";
+import React, { useRef, useEffect } from "react";
+import { motion } from "framer-motion";
+import { bios } from "../../../Data";
+// import portfolio from "../../../assets/portfolio.jpg";
+import BrianMwenda from "./BrianMwenda.pdf";
+import createGlobe from "cobe";
 
-import { motion } from 'framer-motion';
-import { bios } from '../../../Data';
-import portfolio from "../../../assets/portfolio.jpg"
-import BrianMwenda from './BrianMwenda.pdf'
 const About = () => {
+  const canvasRef = useRef();
 
+  useEffect(() => {
+    let phi = 0;
+
+    const globe = createGlobe(canvasRef.current, {
+      devicePixelRatio: 2,
+      width: 600 * 2,
+      height: 600 * 2,
+      phi: 0,
+      theta: 0,
+      dark: 1,
+      diffuse: 1.2,
+      mapSamples: 16000,
+      mapBrightness: 6,
+      baseColor: [0.3, 0.3, 0.3],
+      markerColor: [0.1, 0.8, 1],
+      glowColor: [140 / 255, 18 / 255, 240 / 255],
+      markers: [
+        // longitude latitude
+        { location: [-1.3032036, 36.6825841], size: 0.05 },
+      ],
+      onRender: (state) => {
+        // Called on every animation frame.
+        // `state` will be an empty object, return updated params.
+        state.phi = phi;
+        phi += 0.01;
+      },
+    });
+
+    return () => {
+      globe.destroy();
+    };
+  }, []);
 
   return (
-    <div className="container " id='about'>
+    <div className="container " id="about">
       <motion.div
         initial={{ opacity: 0 }}
         whileInView={{ y: [-50, 0], opacity: 1 }}
-        className="title">
+        className="title"
+      >
         <span>Who Am I?</span>
         <h1>About Me</h1>
       </motion.div>
@@ -23,43 +58,57 @@ const About = () => {
           initial={{ x: 0, opacity: 0 }}
           whileInView={{ x: [-250, 0], opacity: 1 }}
           transition={{ duration: 1 }}
-          className="about_left">
-          <motion.img src={portfolio}
+          className=""
+        >
+          <canvas
+            ref={canvasRef}
+            style={{
+              maxWidth: "100%",
+
+              aspectRatio: 1,
+            }}
+          />
+          {/* <motion.img
+            src={portfolio}
             whileHover={{ y: -48, x: -55 }}
             transition={{ duration: 0.3 }}
-          />
+          /> */}
         </motion.div>
-        <motion.div className="about_right"
+        <motion.div
+          className="about_right"
           initial={{ x: 0, opacity: 0 }}
           whileInView={{ x: [250, 0], opacity: 1 }}
           transition={{ duration: 1 }}
         >
-
-          <p>I`m a software developer with the skills to make payment integrations (eg:darajaAPI,Stripe,Authorize) and many others.
-             I do both Back-end/Front-end development and also most of all develop highly secure, fast and scalable Restful API for your application. </p>
-          {bios.map(bio => {
+          <p>
+            I`m a software developer with the skills to make payment
+            integrations (eg:darajaAPI,Stripe,Authorize) and many others. I do
+            both Back-end/Front-end development and also most of all develop
+            highly secure, fast and scalable Restful API for your application.{" "}
+          </p>
+          {bios.map((bio) => {
             return (
               <div className="bio" key={bio.id}>
-                <span className='bioKey'>{bio.icon}{bio.key}</span>
-                <span className='bioValue'>{bio.value}</span>
+                <span className="bioKey">
+                  {bio.icon}
+                  {bio.key}
+                </span>
+                <span className="bioValue">{bio.value}</span>
               </div>
-            )
+            );
           })}
-         <motion.a href={BrianMwenda} download
+          <motion.a
+            href={BrianMwenda}
+            download
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.3 }}
           >
             Download Resume
-      
-                  
-         
           </motion.a>
         </motion.div>
-
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
